a337ccd5dda3c994621864bf6079fe4d3df1e35f
Leonardo9507 <leonardo.soto2@upr.edu>
2025-09-08 00:02:42 -0400

    Extra task completed: Added music Fixed ball speed to increase when hitting the paddle

--- Game.py
+++ Game.py
@@ -19,7 +19,7 @@ def ball_movement():
 
     # Start the ball movement when the game begins
     # TODO Task 5 Create a Merge Conflict
-    speed = 10
+    speed = 5
     if start:
         ball_speed_x = speed * random.choice((1, -1))  # Randomize initial horizontal direction
         ball_speed_y = speed * random.choice((1, -1))  # Randomize initial vertical direction
@@ -31,7 +31,7 @@ def ball_movement():
             # TODO Task 2: Fix score to increase by 1
             score += 1 # Increase player score
             ball_speed_y *= -1  # Reverse ball's vertical direction
-            speed_increase = 5
+            ball_speed_y += -1
             # TODO Task 6: Add sound effects HERE
             hit_sound = pygame.mixer.Sound(os.path.join('metalpipe.wav'))
             hit_sound.play()
